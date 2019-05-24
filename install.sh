4ea13281cf7091178ca28a83f4c1ccacae5326ff
Powwu // James <leopardsarekittens522@gmail.com>
2019-05-24 16:48:28 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
-sudo -i
-apt-get install libguac-client-rdp0 -y
-apt-get install guacamole -y
-apt-get install xrdp -y
-apt-get install xorgxrdp -y
-apt-get install cinnamon -y
-apt-get install freerdp -y
-rmdir /var/lib/tomcat8/webapps/ROOT
-mv ./lib/ROOT.war /var/lib/tomcat8/webapps
-dpkg -i ./lib/shticker-book-rewritten_1.2.1_amd64.deb
-apt-get install -f
-rm /etc/guacamole/user-mapping.xml
-mv ./lib/user-mapping.xml /etc/guacamole
-rm /etc/xrdp/startwm.sh
-mv ./lib/startwm.sh /etc/xrdp
-/etc/init.d/tomcat8 restart
-/etc/init.d/guacd start
+sudo apt-get install -y libguac-client-rdp0
+wait
+sudo apt-get install -y guacamole
+wait
+sudo apt-get install -y xrdp
+wait
+sudo apt-get install -y xorgxrdp
+wait
+sudo apt-get install -y cinnamon
+wait
+sudo apt-get install -y freerdp
+wait
+sudo rmdir /var/lib/tomcat8/webapps/ROOT
+wait
+sudo mv ./lib/ROOT.war /var/lib/tomcat8/webapps
+wait
+sudo dpkg -i ./lib/shticker-book-rewritten_1.2.1_amd64.deb
+wait
+sudo apt-get install -f
+wait
+sudo rm /etc/guacamole/user-mapping.xml
+wait
+cd ~/Desktop/ttr-web/lib/
+wait
+sudo mv user-mapping.xml /etc/guacamole
+wait
+sudo rm /etc/xrdp/startwm.sh
+wait
+cd ~/Desktop/ttr-web/lib/
+wait
+sudo mv startwm.sh /etc/xrdp
+wait
+sudo /etc/init.d/tomcat8 restart
+wait
+sudo /etc/init.d/guacd start
+wait
 
 
 echo "Script completed! Reboot your server and continue following the steps in the README.md."
