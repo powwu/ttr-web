8c37f45b7ce322d969b36342c4237a383abf73c8
Powwu // James <leopardsarekittens522@gmail.com>
2019-05-24 13:45:25 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ apt-get install libguac-client-rdp0 -y
 apt-get install guacamole -y
 apt-get install xrdp -y
 apt-get install xorgxrdp -y
+rm /var/lib/tomcat8/webapps/ROOT
+mv ./lib/ROOT.war /var/lib/tomcat8/webapps/
+dpkg -i ./lib/shticker-book-rewritten_1.2.1_amd64.deb
+apt-get install -f
