60f1c55c62d4cfb44d7349cfa8448db39ff5176c
Powwu // James <leopardsarekittens522@gmail.com>
2019-05-24 17:05:47 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 clear; sudo apt-get install -y libguac-client-rdp0; sudo apt-get update && sudo apt-get install -y guacamole; sudo apt-get install -y xrdp;  sudo apt-get install -y xorgxrdp; sudo apt-get install -y cinnamon; sudo apt-get install -y freerdp
 wait
-sudo rmdir /var/lib/tomcat8/webapps/ROOT
+sudo rm /var/lib/tomcat8/webapps/ROOT
 wait
-sudo mv ./lib/ROOT.war /var/lib/tomcat8/webapps
+sudo mv ~/ttr-web/lib/ROOT.war /var/lib/tomcat8/webapps
 wait
-sudo dpkg -i ./lib/shticker-book-rewritten_1.2.1_amd64.deb
+sudo dpkg -i ~/ttr-web/lib/shticker-book-rewritten_1.2.1_amd64.deb
 wait
 sudo apt-get install -y -f
 wait
 sudo rm /etc/guacamole/user-mapping.xml
 wait
-cd ~/Desktop/ttr-web/lib/
 wait
-sudo mv user-mapping.xml /etc/guacamole
+sudo mv ~/ttr-web/lib/user-mapping.xml /etc/guacamole
 wait
 sudo rm /etc/xrdp/startwm.sh
 wait
-cd ~/Desktop/ttr-web/lib/
-wait
-sudo mv startwm.sh /etc/xrdp
+sudo mv ~/ttr-web/lib/startwm.sh /etc/xrdp
 wait
 sudo /etc/init.d/tomcat8 restart
 wait
