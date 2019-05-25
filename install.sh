eef632bc71196d1399d2d2aec7c9f6393452578b
Powwu // James <leopardsarekittens522@gmail.com>
2019-05-25 12:34:09 -0400

    Various changes. MV -> CP, xfce4 config & more

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-clear; sudo apt-get install -y libguac-client-rdp0; sudo apt-get update && sudo apt-get install -y guacamole; sudo apt-get install -y xrdp;  sudo apt-get install -y xorgxrdp; sudo apt-get install xserver-xorg-dev
+clear; sudo apt-get install -y libguac-client-rdp0; sudo apt-get update && sudo apt-get install -y guacamole; sudo apt-get install -y xfce4; sudo apt-get install -y xrdp;  sudo apt-get install -y xorgxrdp; sudo apt-get install xserver-xorg-dev
 wait
-sudo rm /var/lib/tomcat8/webapps/ROOT
+sudo rm /var/lib/tomcat8/webapps/ROOT/*
 wait
-sudo mv ~/ttr-web/lib/ROOT.war /var/lib/tomcat8/webapps
+sudo rmdir /var/lib/tomcat8/webapps/ROOT
+wait
+sudo cp ~/ttr-web/lib/ROOT.war /var/lib/tomcat8/webapps
 wait
 sudo dpkg -i ~/ttr-web/lib/shticker-book-rewritten_1.2.1_amd64.deb
 wait
@@ -11,17 +13,22 @@ sudo apt-get install -y -f
 wait
 sudo rm /etc/guacamole/user-mapping.xml
 wait
-wait
-sudo mv ~/ttr-web/lib/user-mapping.xml /etc/guacamole
+sudo cp ~/ttr-web/lib/user-mapping.xml /etc/guacamole
 wait
 sudo rm /etc/xrdp/startwm.sh
 wait
-sudo mv ~/ttr-web/lib/startwm.sh /etc/xrdp
+sudo cp ~/ttr-web/lib/startwm.sh /etc/xrdp
 wait
+sudo rm ~/.config/xfce4/*
+wait
+sudo rmdir ~/.config/xfce4
+wait
+sudo cp ~/ttr-web/lib/.config/xfce4 ~/.config/
 sudo /etc/init.d/tomcat8 restart
 wait
 sudo /etc/init.d/guacd start
 wait
+sudo /etc/init.d/xrdp restart
 
 
 echo "Script completed! Reboot your server and continue following the steps in the README.md."
