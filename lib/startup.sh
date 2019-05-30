fd0dfc8d748de5ded7579d39d4356e08c3b2ad9a
Powwu // James <leopardsarekittens522@gmail.com>
2019-05-30 08:59:37 -0400

    Create startup.sh

---
+++ lib/startup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo rm ~/ToontownRewritten/settings.json; sudo rm ~/Desktop/Terminal.desktop; sudo rm ~/Desktop/Shticker.desktop; sudo cp ~/ttr-web/lib/Terminal.desktop ~/Desktop/; sudo cp ~/ttr-web/lib/Shticker.desktop ~/Desktop/
